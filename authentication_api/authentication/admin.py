7d7a095dd1700b118f2c5dfc0cc0d642f7c07a99
Samiran <samiransarkar513@gmail.com>
2023-06-26 23:10:57 +0530

    course api created and login api fixes done

--- authentication_api/authentication/admin.py
+++ authentication_api/authentication/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Role, UserRole
+from .models import User, Role, UserRole, Courses, CourseUsers
 
 
 class UserRoleInline(admin.TabularInline):
@@ -12,3 +12,6 @@ class UserAdmin(admin.ModelAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Role)
+admin.site.register(UserRole)
+admin.site.register(Courses)
+admin.site.register(CourseUsers)
